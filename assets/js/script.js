de44366fbbe880f549e80d31bf8d5b781a849c9c
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-01-23 17:46:23 +0530

    html: slick slider links added and slick folder added in vendor, css: slick slider styling done. Header and banner styling completed, JS: Slick slider plugin jQuery code written

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,13 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function () {
+    $('.slider1').slick({
+        nextArrow: false,
+        prevArrow: false,
+        dots: true,
+        speed: 800,
+        slideToShow: 1,
+        autoplay: false,
+        customPaging: function (slider, i) {
+            return '<span class="fas fa-circle dot"></span>';
+        }
+    });
+});
