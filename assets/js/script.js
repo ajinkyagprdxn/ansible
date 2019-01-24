7c034448ae4138ede219491ee281bb34077d099d
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-01-24 16:05:10 +0530

    Lightbox for maps added with jQuery and Lightbox plugin added for viewing images

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,34 @@ $(document).ready(function () {
             return '<span class="fas fa-circle dot"></span>';
         }
     });
+
+    var disp = document.getElementById("openMod");
+    var bigdisp = $("#modDisplay");
+
+    var bodyh = $("body");
+
+    $("#closebtn").click(function () {
+        closefn();
+    });
+
+    var open = $(".opendata");
+
+    for (var i = 0; i < open.length; i++) {
+        open[i].addEventListener("click", function (e) {
+            disp.classList.replace('modalImage', 'openModal');
+            bigdisp.html(this.innerHTML);
+            bodyh.addClass("bodytemp");
+        });
+    }
+
+    bigdisp.click(function (event) {
+        event.stopPropagation();
+    });
+
+    disp.addEventListener("click", closefn3);
+    function closefn3() {
+        disp.classList.replace('openModal', 'modalImage');
+        bodyh.classList.remove("bodytemp");
+    }
 });
+
